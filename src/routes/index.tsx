72d56d6e01622b8dbd8af8c35fab216da65b10e3
김가영 <namiein@naver.com>
2023-04-28 22:39:21 +0900

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import BaseLayout from 'features/Layout/BaseLayout';
 import Home from 'pages/Home';
 
 export default function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/" element={<BaseLayout />}>
+                    <Route index element={<Home />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     );
