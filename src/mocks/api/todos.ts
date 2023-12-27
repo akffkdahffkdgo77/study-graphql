f8342162057bf79a2724bab4142cc4a77f885d02
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2023-12-27 18:12:48 +0900

    feat : mock api refresh token 추가

--- src/mocks/api/todos.ts
+++ src/mocks/api/todos.ts
@@ -4,13 +4,29 @@ import { v4 as uuidv4 } from 'uuid';
 import { todoList } from 'mocks/api/data/todos';
 
 const handlers = [
-    graphql.query('TokenTest', (req, res, ctx) => {
-        return res(ctx.errors([{ extensions: { code: 'UNAUTHENTICATED' }, message: 'Unauthorized' }]));
+    graphql.mutation('RefreshToken', (req, res, ctx) => {
+        return res(
+            ctx.data({
+                refreshToken: {
+                    refreshToken: uuidv4(),
+                    accessToken: uuidv4()
+                }
+            })
+        );
     }),
     graphql.query('GetTodos', (req, res, ctx) => {
+        // Token이 없다면
+        if (!req.headers.get('authorization')) {
+            return res(ctx.errors([{ extensions: { code: 'UNAUTHENTICATED' }, message: 'Unauthorized' }]));
+        }
         return res(ctx.data(todoList));
     }),
     graphql.mutation('CreateTodo', (req, res, ctx) => {
+        // Token이 없다면
+        if (!req.headers.get('authorization')) {
+            return res(ctx.errors([{ extensions: { code: 'UNAUTHENTICATED' }, message: 'Unauthorized' }]));
+        }
+
         const { title, description, image, tag } = req.variables.input;
         return res(
             ctx.data({
